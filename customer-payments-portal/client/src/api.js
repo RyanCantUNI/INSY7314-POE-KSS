e1f5168fb7ce74471885113d2f0e47af2694bcca
ST10384544 <ST10384544@vcconnect.edu.za>
2025-10-08 09:16:19 +0200

    refactored backend to remove the sql stuff

--- customer-payments-portal/client/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const client = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE || 'https://api.example.com',
-  withCredentials: true // send/receive cookies
-});
-
-export default client;

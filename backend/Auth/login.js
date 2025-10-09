839ee9821aabb5254a60ba3b71fd902f3db395ec
ST10384544 <ST10384544@vcconnect.edu.za>
2025-10-09 21:49:41 +0200

    login working I hope

--- backend/Auth/login.js
+++ backend/Auth/login.js
@@ -1,30 +1,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+import { client } from "../DB/db.js";
+
 
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 
+//db collection
+let collection = client.db("APDS").collection("users");
+
 // Login route
 app.post("/login", async (req, res) => {
-    const { name, password } = req.body;
+    const { email, password } = req.body;
     try {
-        const user = await collection.findOne({ name: name });
+        const user = await collection.findOne({ email: email });
         if (!user) {
-            return res.status(401).send("Ensure name and password are correct.");
+            return res.status(401).send("User already exists.");
         }
+        else{
         const isPasswordValid = await bcrypt.compare(password, user.password);
         if (!isPasswordValid) {
             return res.status(401).send("Ensure name and password are correct.");
         }
         else {
-            const token = jwt.sign({ name: name }, "User token", { expiresIn: '2h' });
+            const token = jwt.sign({ email: email }, "User token", { expiresIn: '2h' });
             res.status(200).send("Successfully logged in");
             console.log(token);
         }
-    } catch (error) {
+        }
+    }
+    catch (error) {
         console.error(error);
         res.status(401).send("Failed login attempt.");
     }

8d4556a2db4641fa9669cfbfb1655d1cbf706031
RyanCantUNI <ST10301651@vcconnect.edu.za>
2025-10-09 13:51:31 +0200

    removed server from login

--- backend/Auth/login.js
+++ backend/Auth/login.js
@@ -30,7 +30,3 @@ app.post('/login', (req, res) => {
     res.json({ message: 'Login successful', token });
 });
 
-// Start server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});

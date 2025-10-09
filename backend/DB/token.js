d3e8b45154bc649c24639b5e4a39889e1525924e
ST10384544 <ST10384544@vcconnect.edu.za>
2025-10-09 22:50:11 +0200

    payments work with flase UUID

--- backend/DB/token.js
+++ backend/DB/token.js
@@ -5,12 +5,14 @@ const checkToken = (req,res,next) =>
 {
     try{
         const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "Current user");
+        jwt.verify(token, "User token");
         next();
 
     }
-    catch{
+    catch(err){
+        
         res.send("invalid token")
+        console.log("Token Error", err)
     }
 }
 

2cdffa1897e7e6def66a728777061068aa32db10
ST10384544 <ST10384544@vcconnect.edu.za>
2025-11-04 20:20:06 +0200

    added basic schema to adminModel

--- backend/models/adminModel.js
+++ backend/models/adminModel.js
@@ -2,5 +2,68 @@
 class summary 
 this is a template class for admin users in the mongo
 like mvc same same but different 
-
+Admind :{
+    id: String,
+    name: String,
+    email: String,
+    password: String,
+    role: String}
 */
+
+//for data definition language 
+const mongoose = require("mongoose");
+
+//db schema
+
+//TO:DO and more ECC
+const adminSchema =  new mongoose.Schema({
+    id: {
+        //auto generated 
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        validate: {
+            //regex for checking name input
+        validator: function (v) {
+          let regex = new RegExp(
+            "^(?=[a-zA-Z0-9._ ]{10,35}$)(?!.*[_.]{2})[^_.].*[^_.]$"
+            
+          );
+          return regex.test(v);
+        },
+        required: true
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+         validate: {
+            //regex checker for email input 
+        validator: function (v) {
+          let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+          return regex.test(v);
+        },
+    
+        unique: true
+    }
+    },
+    password: {
+        //need to salt and ecypt
+        type: String,
+        required: true
+    },
+    role: {
+        //automatically set when user accounts are created 
+        type: String,
+        required: true
+    }
+});
+
+
+
+//variable to contain our schema --like a model class in mvc
+const adminModel = mongoose.model("admin", adminSchema);
+export default adminModel  
--- backend/models/customerModel.js
+++ backend/models/customerModel.js
@@ -4,3 +4,4 @@ this is a template class for customer users in the mongo
 like mvc same same but different 
 
 */
+

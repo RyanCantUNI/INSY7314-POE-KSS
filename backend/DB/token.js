c733923308dc386a408dee4b8f5fb6249dff490f
ST10384544 <ST10384544@vcconnect.edu.za>
2025-11-04 20:03:20 +0200

    rework the jwt token to use access roles and id

--- backend/DB/token.js
+++ backend/DB/token.js
@@ -1,19 +1,31 @@
 //token handlers modules 
 import jwt from "jsonwebtoken";
 
-const checkToken = (req,res,next) =>
+const userToken = (id ,email)=>
 {
-    try{
-        const token = req.headers.authorization.split(" ")[1];
-        jwt.verify(token, "User token");
-        next();
 
-    }
-    catch(err){
-        
-        res.send("invalid token")
-        console.log("Token Error", err)
-    }
+    //creates base user token with no permissions 
+    //for customers
+    /*
+    take in user id and email and sign them onto the token 
+    id for current logged in user
+    email for verfication perposes
+    */
+   return jwt.sign({ id, email }, process.env.JWT_SECRET);;
 }
 
-export default checkToken
+
+const generateAdminsToken = (id, email, role) => {
+
+    /*
+    for signed users with access rolls
+    id for current logged in user
+    email for verfication perposes
+    role for admin access roles
+    */
+
+
+  return jwt.sign({ id, email, role }, process.env.JWT_SECRET);
+};
+
+export default {userToken, generateAdminsToken}

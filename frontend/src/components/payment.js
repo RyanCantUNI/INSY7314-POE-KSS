bb719000c5e63fe12713861d11d128f149dd3f53
RyanCantUNI <ST10301651@vcconnect.edu.za>
2025-11-06 23:37:24 +0200

    pipeline test #5 --fixes

--- frontend/src/components/payment.js
+++ frontend/src/components/payment.js
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
-import axios, { Axios } from "axios";
+import { useState } from "react";
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 
 
 const AddPayment = () => {
-    const userID = localStorage.getItem("userID");
     const [amount, setAmount] = useState('');
     const [providerAccount, setProviderAccount] = useState('');
     const [currency, setCurrency] = useState('');

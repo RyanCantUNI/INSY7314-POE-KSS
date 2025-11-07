3728013d76b53085b2aa3152e838203e2f0aebef
Dharvish Toolsi <ST10275490@vcconnect.edu.za>
2025-11-07 21:23:22 +0200

    Fixed data links

--- backend/Auth/register.js
+++ backend/Auth/register.js
@@ -54,8 +54,8 @@ user.use(bodyParser.json())
 //         })
 //         //push to db
 //         await admin.save();
-    
-        
+
+
 
 //   } catch (error) {
 //     console.error(error);
@@ -75,16 +75,16 @@ user.use(bodyParser.json())
 //delet acount
 
 //register standard user 
-user.post("/register/customer",[
+user.post("/register/customer", [
     //sanitation of inputs
-        body("email").isEmail(),
-        body("password").isLength({ min: 8 }),
-        body("customer_name").notEmpty(),
-        body("national_Id").isLength({ min: 8, max: 20 }),
-        body("bankaccount").notEmpty(),
-        body("accountnumber").isLength({ min: 8, max: 20 })
+    body("email").isEmail(),
+    body("password").isLength({ min: 8 }),
+    body("fullName").notEmpty(),
+    body("nationalId").isLength({ min: 8, max: 20 }),
+    body("bankCode").notEmpty(),
+    body("accountNumber").isLength({ min: 8, max: 20 })
 ], async (req, res) => {
-    try{
+    try {
         /*
        customer {
 id:
@@ -107,11 +107,11 @@ role
         */
         //encrypting and salting password
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const customerName = req.body.customer_name
-        const _national_Id = req.body.national_Id
-        const _bankaccount = req.body.bankaccount
-        const _accountnumber = req.body.accountnumber
-    
+        const customerName = req.body.fullName
+        const _national_Id = req.body.nationalId
+        const _bankaccount = req.body.bankCode
+        const _accountnumber = req.body.accountNumber
+
         const _id = new mongoose.Types.ObjectId();
         let emailIn = req.body.email
         //creating new admin entry 
@@ -124,18 +124,18 @@ role
             email: emailIn,
             password: hashedPassword,
             role: "customer"
-    })
+        })
         //push to db
         await customer.save();
-    
-        
+
+
         res.status(201).json({ message: "Customer registered successfully" });
         console.log(customer)
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error" });
+    }
 });
 
 
@@ -143,47 +143,47 @@ role
 //register admin user
 user.post("/register/admin",
     [
-    //sanitation of inputs
+        //sanitation of inputs
         body("email").isEmail(),
         body("password").isLength({ min: 8 }),
-        body("admin_name").notEmpty()
+        body("fullName").notEmpty()
     ], async (req, res) => {
-    try{
-        /*
-        id: String,
-        name: String,
-        email: String,
-        password: String,
-        role: String}
-        */
-        //encrypting and salting password
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        const _adminName = req.body.admin_name
-       
-        const _id = new mongoose.Types.ObjectId();
-        let emailIn = req.body.email
-        
-        //creating new admin entry 
-        const admin = new Admin({
-            id: _id,
-            admin_name: _adminName,
-            email: emailIn,
-            password: hashedPassword,
-            role: "admin"
+        try {
+            /*
+            id: String,
+            name: String,
+            email: String,
+            password: String,
+            role: String}
+            */
+            //encrypting and salting password
+            const hashedPassword = await bcrypt.hash(req.body.password, 10)
+            const _adminName = req.body.fullName
 
-        })
-        //push to db
-        await admin.save();
-    
-        
-        res.status(201).json({ message: "Admin registered successfully" });
-        console.log(admin)
+            const _id = new mongoose.Types.ObjectId();
+            let emailIn = req.body.email
 
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Internal server error" });
-  }
-});
+            //creating new admin entry 
+            const admin = new Admin({
+                id: _id,
+                admin_name: _adminName,
+                email: emailIn,
+                password: hashedPassword,
+                role: "admin"
+
+            })
+            //push to db
+            await admin.save();
+
+
+            res.status(201).json({ message: "Admin registered successfully" });
+            console.log(admin)
+
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Internal server error" });
+        }
+    });
 //
 
 
@@ -192,7 +192,7 @@ user.get("/getuser", async (_, res) => {
     try {
         const users = await Admin.find();
         const customer = await Customer.find();
-       res.json({ admins: users, customers: customer});
+        res.json({ admins: users, customers: customer });
     } catch (error) {
         console.error("Error fetching users:", error);
         res.status(500).json({ message: "Internal server error" });
@@ -206,10 +206,10 @@ user.get("/getuser", async (_, res) => {
 
 //delete account 
 //find user account by id then delete it
-user.delete("deletuser/id",[
+user.delete("deletuser/id", [
     //sanitation of inputs
     body("id").notEmpty()
-],async (req, res) => {
+], async (req, res) => {
     try {
         const userId = req.params.id;
         const _admin = await Admin.findByIdAndDelete(userId);
@@ -217,7 +217,7 @@ user.delete("deletuser/id",[
         if (!_admin && !_customer) {
             return res.status(404).json({ message: "User not found" });
         }
-       
+
         res.json({ message: "User deleted successfully" });
     } catch (error) {
         console.error("Error deleting user:", error);
